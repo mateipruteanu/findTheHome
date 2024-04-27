2634273f4a4387c14745475c251f9c8ab040ac49
mateipruteanu <mateipruteanu1@gmail.com>
2024-04-27 10:51:37 +0300

    [FE] added admin check for admin routes in middleware

--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -1,11 +1,22 @@
 import { NextRequest, NextResponse } from "next/server";
-import { Routes, isPrivateRoute } from "./routes";
+import { Routes, isAdminRoute, isPrivateRoute } from "./routes";
+import { jwtDecode } from "jwt-decode";
+import { DecodedToken } from "./AuthProvider";
 
 export default async function middleware(request: NextRequest) {
   const token = request.cookies.get("access_token");
+  const decoded: DecodedToken | undefined = token
+    ? jwtDecode(token.value.toString())
+    : undefined;
   const pathname = request.nextUrl.pathname;
 
   if ((pathname === Routes.LOGIN || pathname === Routes.REGISTER) && token) {
+    const newUrl = request.nextUrl.clone();
+    newUrl.pathname = Routes.HOME;
+    return NextResponse.redirect(newUrl);
+  }
+
+  if (isAdminRoute(pathname) && decoded?.role.toLowerCase() !== "admin") {
     const newUrl = request.nextUrl.clone();
     newUrl.pathname = Routes.HOME;
     return NextResponse.redirect(newUrl);
